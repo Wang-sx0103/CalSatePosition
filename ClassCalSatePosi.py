6ad741d47ad660f43a2ddbb7f1483ae5ca43b87b
Alex <1321312087@qq.com>
2021-05-10 09:23:07 +0800

    fix bugs

--- ClassCalSatePosi.py
+++ ClassCalSatePosi.py
@@ -2,68 +2,65 @@
 import math
 
 
-class CalSatePosi:
-    __satNum = 0
-    fileName = ""
-    __satDict = {}
-    __hour = 24
-    __allData = []
-    note = []
-    __Day_of_Year = ""
-    # ORBIT0
-    af0 = [[0]*__hour for i in range(__satNum)]
-    af1 = [[0]*__hour for i in range(__satNum)]
-    af2 = [[0]*__hour for i in range(__satNum)]
-    # ORBIT1
-    aode = [[0]*__hour for i in range(__satNum)]
-    crs = [[0]*__hour for i in range(__satNum)]
-    deltan = [[0]*__hour for i in range(__satNum)]
-    m0 = [[0]*__hour for i in range(__satNum)]
-    # ORBIT2
-    cuc = [[0]*__hour for i in range(__satNum)]
-    e = [[0]*__hour for i in range(__satNum)]
-    cus = [[0]*__hour for i in range(__satNum)]
-    roota = [[0]*__hour for i in range(__satNum)]
-    # ORBIT3
-    toe = [[0]*__hour for i in range(__satNum)]
-    cic = [[0]*__hour for i in range(__satNum)]
-    omega0 = [[0]*__hour for i in range(__satNum)]
-    cis = [[0]*__hour for i in range(__satNum)]
-    # ORBIT4
-    i0 = [[0]*__hour for i in range(__satNum)]
-    crc = [[0]*__hour for i in range(__satNum)]
-    omega = [[0]*__hour for i in range(__satNum)]
-    omegadot = [[0]*__hour for i in range(__satNum)]
-    # ORBIT5
-    idot = [[0]*__hour for i in range(__satNum)]
-    wn = [[0]*__hour for i in range(__satNum)]
-    # ORBIT6
-    precise = [[0]*__hour for i in range(__satNum)]
-    health = [[0]*__hour for i in range(__satNum)]
-    tgd = [[0]*__hour for i in range(__satNum)]
-    idoc = [[0]*__hour for i in range(__satNum)]
-    # ORBIT7
-    starsec = [[0]*__hour for i in range(__satNum)]
-    tlong = [[0]*__hour for i in range(__satNum)]
-    X = [[0]*2880 for i in range(__satNum)]
-    Y = [[0]*2880 for i in range(__satNum)]
-    Z = [[0]*2880 for i in range(__satNum)]
-    __GM = 3.986004E14
-    __Ve = 7.2921151467E-14
-    __pi = 3.1415926535897932
-    __mu = 3.9860047000E14
-
+class CalSatePosi(object):
     def __init__(self, fileName, satNum):
-        self.__fileDir = fileName
+        self.fileName = fileName
         self.__satNum = satNum
-        self.__Day_of_Year = fileName[-1][12:19]
+        __hour = 24
+        __satNum = satNum
+        self.__allData = []
+        self.note = []
+        self.__satDict = {}
+        # ORBIT0
+        self.__af0 = [[0]*__hour for i in range(__satNum)]
+        self.__af1 = [[0]*__hour for i in range(__satNum)]
+        self.__af2 = [[0]*__hour for i in range(__satNum)]
+        # ORBIT1
+        self.__aode = [[0]*__hour for i in range(__satNum)]
+        self.__crs = [[0]*__hour for i in range(__satNum)]
+        self.__deltan = [[0]*__hour for i in range(__satNum)]
+        self.__m0 = [[0]*__hour for i in range(__satNum)]
+        # ORBIT2
+        self.__cuc = [[0]*__hour for i in range(__satNum)]
+        self.__e = [[0]*__hour for i in range(__satNum)]
+        self.__cus = [[0]*__hour for i in range(__satNum)]
+        self.__roota = [[0]*__hour for i in range(__satNum)]
+        # ORBIT3
+        self.__toe = [[0]*__hour for i in range(__satNum)]
+        self.__cic = [[0]*__hour for i in range(__satNum)]
+        self.__omega0 = [[0]*__hour for i in range(__satNum)]
+        self.__cis = [[0]*__hour for i in range(__satNum)]
+        # ORBIT4
+        self.__i0 = [[0]*__hour for i in range(__satNum)]
+        self.__crc = [[0]*__hour for i in range(__satNum)]
+        self.__omega = [[0]*__hour for i in range(__satNum)]
+        self.__omegadot = [[0]*__hour for i in range(__satNum)]
+        # ORBIT5
+        self.__idot = [[0]*__hour for i in range(__satNum)]
+        self.__wn = [[0]*__hour for i in range(__satNum)]
+        # ORBIT6
+        self.__precise = [[0]*__hour for i in range(__satNum)]
+        self.__health = [[0]*__hour for i in range(__satNum)]
+        self.__tgd = [[0]*__hour for i in range(__satNum)]
+        self.__idoc = [[0]*__hour for i in range(__satNum)]
+        # ORBIT7
+        self.__starsec = [[0]*__hour for i in range(__satNum)]
+        self.__tlong = [[0]*__hour for i in range(__satNum)]
+        self.X = [[0]*2880 for i in range(__satNum)]
+        self.Y = [[0]*2880 for i in range(__satNum)]
+        self.Z = [[0]*2880 for i in range(__satNum)]
+        self.__GM = 3.986004E14
+        self.__Ve = 7.2921151467E-14
+        self.__pi = 3.1415926535897932
+        self.__mu = 3.9860047000E14
+        self.__Day_of_Year = fileName[12:19]
         self.__readFile()
         self.__createDict()
         self.__sort()
         self.__calSatPosition()
 
     def __readFile(self):
-        with open(self.fileName, "r", encoding="utf-8") as file:
+        with open("./data/"+self.fileName, "r", encoding="utf-8") as file:
             self.__allData = file.readlines()
 
     def __createDict(self):
@@ -136,7 +133,7 @@ class CalSatePosi:
                 self.__idot[satNum][hour] = \
                     self.__str2float(self.__allData[row+5][4:23])
                 self.__wn[satNum][hour] = \
-                    self.__tr2float(self.__allData[row+5][42:61])
+                    self.__str2float(self.__allData[row+5][42:61])
 
                 self.__precise[satNum][hour] = \
                     self.__str2float(self.__allData[row+6][4:23])
@@ -161,52 +158,52 @@ class CalSatePosi:
                 T = 432000 + 30 * epoch
                 time = 0
                 for hour in range(24):
-                    if abs(T-self.toe[oneNum][hour]) <= 1800:
+                    if abs(T-self.__toe[oneNum][hour]) <= 1800:
                         time = hour
-                sqrat = self.roota[oneNum][time]
+                sqrat = self.__roota[oneNum][time]
                 a = sqrat**2
                 if a == 0.0:
                     continue
                 n0 = pow(self.__GM/(a**3), 0.5)
-                n = n0 + self.deltan[oneNum][time]
-                tk = T - self.toe[oneNum][time] - 14.0
+                n = n0 + self.__deltan[oneNum][time]
+                tk = T - self.__toe[oneNum][time] - 14.0
                 if tk > 302400:
                     tk = tk - 604800
                 elif tk < -302400:
                     tk = tk + 604800
-                mk = self.m0[oneNum][time] + n * tk
+                mk = self.__m0[oneNum][time] + n * tk
                 ek1 = mk
                 ek2 = 1E10
                 while abs(ek2-ek1) > 1E-20:
-                    ek2 = mk + self.e[oneNum][time]*math.sin(ek1)
+                    ek2 = mk + self.__e[oneNum][time]*math.sin(ek1)
                     ek1 = ek2
                 Ek = ek1
-                sinfk = math.sqrt(1-(self.e[oneNum][time])**2)*math.sin(Ek) / \
-                    (1 - (self.e[oneNum][time])*math.cos(Ek))
-                cosfk = (math.cos(Ek) - self.e[oneNum][time]) / \
-                    (1 - self.e[oneNum][time]*math.cos(Ek))
+                sinfk = math.sqrt(1-(self.__e[oneNum][time])**2) * \
+                    math.sin(Ek)/(1 - (self.__e[oneNum][time])*math.cos(Ek))
+                cosfk = (math.cos(Ek) - self.__e[oneNum][time]) / \
+                    (1 - self.__e[oneNum][time]*math.cos(Ek))
                 fk = math.atan2(sinfk, cosfk)
-                phik = fk + self.omega[oneNum][time]
-                deltauk = self.cus[oneNum][time]*math.sin(2*phik) + \
-                    self.cus[oneNum][time] * math.cos(2 * phik)
-                deltark = self.crs[oneNum][time]*math.sin(2*phik) + \
-                    self.crs[oneNum][time] * math.cos(2 * phik)
-                deltaik = self.cis[oneNum][time]*math.sin(2*phik) + \
-                    self.cis[oneNum][time] * math.cos(2 * phik)
+                phik = fk + self.__omega[oneNum][time]
+                deltauk = self.__cus[oneNum][time]*math.sin(2*phik) + \
+                    self.__cus[oneNum][time] * math.cos(2 * phik)
+                deltark = self.__crs[oneNum][time]*math.sin(2*phik) + \
+                    self.__crs[oneNum][time] * math.cos(2 * phik)
+                deltaik = self.__cis[oneNum][time]*math.sin(2*phik) + \
+                    self.__cis[oneNum][time] * math.cos(2 * phik)
                 uk = phik + deltauk
-                rk = a * (1 - self.e[oneNum][time])*math.cos(Ek) + deltark
-                ik = self.i0[oneNum][time] + deltaik + \
-                    self.idot[oneNum][time] * tk
+                rk = a * (1 - self.__e[oneNum][time])*math.cos(Ek) + deltark
+                ik = self.__i0[oneNum][time] + deltaik + \
+                    self.__idot[oneNum][time] * tk
                 xk = rk * math.cos(uk)
                 yk = rk * math.sin(uk)
                 if oneNum <= 5:
-                    Lk = self.omega0[oneNum][time] + \
-                        self.omegadot[oneNum][time] * tk - \
-                        self.__Ve * self.toe[oneNum][time]
+                    Lk = self.__omega0[oneNum][time] + \
+                        self.__omegadot[oneNum][time] * tk - \
+                        self.__Ve * self.__toe[oneNum][time]
                     tX1 = xk * math.cos(Lk) - yk * math.cos(ik) * math.sin(Lk)
                     tY1 = xk * math.cos(Lk) + yk * math.cos(ik) * math.cos(Lk)
                     tZ1 = yk * math.sin(ik)
-                    delt_z = self.Ve * self.tk
+                    delt_z = self.__Ve * tk
                     delt_x = -5 * math.pi / 180
                     self.X[oneNum][epoch] = tX1 * math.cos(delt_z) + \
                         tY1 * math.cos(delt_x) * math.sin(delt_z) + \
@@ -217,9 +214,9 @@ class CalSatePosi:
                     self.Z[oneNum][epoch] = (-tY1) * math.sin(delt_x) + \
                         tZ1 * math.cos(delt_x)
                 else:
-                    Lk = self.omega0[oneNum][time] + \
-                        (self.omegadot[oneNum][time] - self.Ve) * \
-                        self.tk - self.Ve * self.toe[oneNum][time]
+                    Lk = self.__omega0[oneNum][time] + \
+                        (self.__omegadot[oneNum][time] - self.__Ve) * \
+                        tk - self.__Ve * self.__toe[oneNum][time]
                     self.X[oneNum][epoch] = xk * math.cos(Lk) - \
                         yk * math.cos(ik) * math.sin(Lk)
                     self.Y[oneNum][epoch] = xk * math.sin(Lk) + \
@@ -241,7 +238,7 @@ class CalSatePosi:
             strDate = strDate + str(days)
         return strDate
 
-    def __str2float(str1):
+    def __str2float(self, str1):
         if str1.strip() == "":
             return 0.0
         else:
